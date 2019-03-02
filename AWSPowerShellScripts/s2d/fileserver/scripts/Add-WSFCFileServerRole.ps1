90e662c85d0db0a8789f718d369361193fe3ce9f
Michael Haken <michael.haken@outlook.com>
2019-03-01 22:11:42 -0500

    Updated parameter sets.

--- AWSPowerShellScripts/s2d/fileserver/scripts/Add-WSFCFileServerRole.ps1
+++ AWSPowerShellScripts/s2d/fileserver/scripts/Add-WSFCFileServerRole.ps1
@@ -8,18 +8,21 @@ Param(
 	[Parameter(ParameterSetName = "CredGeneral", Mandatory = $true)]
 	[Parameter(ParameterSetName = "PassGeneral", Mandatory = $true)]
 	[Parameter(ParameterSetName = "EncryptedPassGeneral", Mandatory = $true)]
+	[Parameter(ParameterSetName = "SSMParameterStoreGeneral", Mandatory = $true)]
 	[ValidateNotNullOrEmpty()]
 	[System.String[]]$IPAddress,
 
 	[Parameter(ParameterSetName = "CredGeneral", Mandatory = $true)]
 	[Parameter(ParameterSetName = "PassGeneral", Mandatory = $true)]
 	[Parameter(ParameterSetName = "EncryptedPassGeneral", Mandatory = $true)]
+	[Parameter(ParameterSetName = "SSMParameterStoreGeneral", Mandatory = $true)]
 	[ValidateNotNullOrEmpty()]
 	[System.String]$VirtualDisk,
 
 	[Parameter(ParameterSetName = "CredSOFS", Mandatory = $true)]
 	[Parameter(ParameterSetName = "PassSOFS", Mandatory = $true)]
 	[Parameter(ParameterSetName = "EncryptedPassSOFS", Mandatory = $true)]
+	[Parameter(ParameterSetName = "SSMParameterStoreSOFS", Mandatory = $true)]
 	[Switch]$SOFS,
 
 	[Parameter(ParameterSetName = "CredSOFS")]
@@ -32,6 +35,8 @@ Param(
 	[Parameter(ParameterSetName = "PassGeneral", Mandatory = $true)]
 	[Parameter(ParameterSetName = "EncryptedPassSOFS", Mandatory = $true)]
 	[Parameter(ParameterSetName = "EncryptedPassGeneral", Mandatory = $true)]
+	[Parameter(ParameterSetName = "SSMParameterStoreSOFS", Mandatory = $true)]
+	[Parameter(ParameterSetName = "SSMParameterStoreGeneral", Mandatory = $true)]
 	[ValidateNotNullOrEmpty()]
 	[System.String]$Username,
 
@@ -42,13 +47,18 @@ Param(
 
 	[Parameter(ParameterSetName = "EncryptedPassSOFS", Mandatory = $true)]
 	[Parameter(ParameterSetName = "EncryptedPassGeneral", Mandatory = $true)]
-	[System.String]$KMSEncryptedPassword
+	[System.String]$KMSEncryptedPassword,
+
+	[Parameter(ParameterSetName = "SSMParameterStoreSOFS", Mandatory = $true)]
+	[Parameter(ParameterSetName = "SSMParameterStoreGeneral", Mandatory = $true)]
+	[ValidateNotNullOrEmpty()]
+	[System.String]$SSMPasswordParameterName
 )
 
 Start-Transcript -Path "$env:ProgramData\Amazon\Logs\$(Split-Path -Path $MyInvocation.MyCommand.Path -Leaf).txt" -Append
 $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
 
-if ($PSCmdlet.ParameterSetName -notlike "*Cred")
+if ($PSCmdlet.ParameterSetName -ne "Cred")
 {
 	if (-not $Username.Contains("\"))
 	{
@@ -56,12 +66,42 @@ if ($PSCmdlet.ParameterSetName -notlike "*Cred")
 		$Username = "$($DomainName.Split(".")[0])\$Username"
 	}
 
-	if ($PSCmdlet.ParameterSetName -like "EncryptedPass*")
+	switch -Wildcard ($PSCmdlet.ParameterSetName)
 	{
-		[System.Byte[]]$Bytes = [System.Convert]::FromBase64String($KMSEncryptedPassword)
-		[System.IO.MemoryStream]$MStream = New-Object -TypeName System.IO.MemoryStream($Bytes, 0, $Bytes.Length)
-		[Amazon.KeyManagementService.Model.DecryptResponse]$Response = Invoke-KMSDecrypt -CipherTextBlob $MStream
-		$Password = [System.Text.Encoding]::UTF8.GetString($Response.PlainText.ToArray())
+		"Pass*" {
+			# Do nothing		
+			break
+		}
+		"EncryptedPass*" {
+			[System.Byte[]]$Bytes = [System.Convert]::FromBase64String($KMSEncryptedPassword)
+			[System.IO.MemoryStream]$MStream = New-Object -TypeName System.IO.MemoryStream($Bytes, 0, $Bytes.Length)
+			[Amazon.KeyManagementService.Model.DecryptResponse]$Response = Invoke-KMSDecrypt -CipherTextBlob $MStream
+			$Password = [System.Text.Encoding]::UTF8.GetString($Response.PlainText.ToArray())
+
+			break
+		}
+		"SSMParameterStore*" {
+			try
+			{
+				[Amazon.SimpleSystemsManagement.Model.GetParametersResponse]$Response =  Get-SSMParameterValue -Name $SSMPasswordParameterName -WithDecryption $true
+
+				if ($Response -eq $null -or ($Response.InvalidParameters -ne $null -and $Response.InvalidParameters.Count -gt 0 -and $Response.InvalidParameters[0] -ieq $SSMPasswordParameterName))
+				{
+					throw "Could not find an SSM Parameter Store Key with a value of $SSMPasswordParameterName."
+				}
+
+				$Password = $Response.Parameters | Select-Object -First 1 -ExpandProperty Value
+			}
+			catch [EXception]
+			{
+				throw "Could not retrieve the SSM Parameter Store Key for the domain password.`r`n$($_.Exception.GetType().FullName)`r`n$($_.Exception.Message)"
+			}
+		
+			break
+		}
+		default {
+			throw "Unknown parameter set $($PSCmdlet.ParameterSetName)."
+		}
 	}
 
 	$Credential = New-Object -TypeName System.Management.Automation.PSCredential($Username, (ConvertTo-SecureString -String $Password -AsPlainText -Force))
